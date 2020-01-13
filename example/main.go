33bbf7638a61570a95dec0d0561f0c89ea1e4d6d
stormlin <lin-jinting@outlook.com>
2020-01-13 11:14:31 +0800

    使用同一接口同时开启 H2 和 H3 服务器

--- example/main.go
+++ example/main.go
@@ -3,25 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/lucas-clemente/quic-go/http3"
-	"golang.org/x/net/http2"
 )
-
-func runH2Server() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	srv := &http.Server{
-		Addr:    "0.0.0.0:443",
-		Handler: http.FileServer(http.Dir(cwd)),
-	}
-	http2.ConfigureServer(srv, &http2.Server{})
-	log.Fatal(srv.ListenAndServeTLS("cert.pem", "cert.key"))
-}
 
 type h3FileHandler struct {
 	handler http.Handler
@@ -39,19 +23,7 @@ func (h *h3FileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.handler.ServeHTTP(w, r)
 }
 
-func runH3Server() {
-	h3srv := http3.Server{
-		Server: &http.Server{
-			Addr:    "0.0.0.0:443",
-			Handler: newH3FileServer(),
-		},
-	}
-	log.Fatal(h3srv.ListenAndServeTLS("cert.pem", "cert.key"))
-}
-
 func main() {
-	// 运行 HTTP2 服务器
-	go runH2Server()
-	// 运行 HTTP3 服务器
-	runH3Server()
+	// 同时运行 HTTP/3 和 HTTP/2 服务器
+	log.Fatal(http3.ListenAndServe("0.0.0.0:443", "cert.pem", "cert.key", newH3FileServer()))
 }
