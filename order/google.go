73e47701142fd2c3af71512306dd6730d73adea9
stormlin <lin-jinting@outlook.com>
2020-02-03 17:34:01 +0800

    add transmission order for sample "google"

--- order/google.go
+++ order/google.go
@@ -1,13 +1,25 @@
 package order
 
 var GoogleNetworkList = []string{
-
+	"index.html",
+	"AA2YrTt2q0pPF6A2L90tdVR6IsduvglADQ.js",
+	"dn.js",
+	"ACT90oH5P7nEVFGmCOcowWVXAqdf2S8xJA.js",
+	"dn.html",
 }
 
 var GooglePerformanceList = []string{
-
+	"index.html",
+	"ACT90oH5P7nEVFGmCOcowWVXAqdf2S8xJA.js",
+	"AA2YrTt2q0pPF6A2L90tdVR6IsduvglADQ.js",
+	"dn.js",
+	"dn.html",
 }
 
 var GoogleNetworkPlusList = []string{
-	
+	"index.html",
+	"AA2YrTt2q0pPF6A2L90tdVR6IsduvglADQ.js",
+	"ACT90oH5P7nEVFGmCOcowWVXAqdf2S8xJA.js",
+	"dn.js",
+	"dn.html",
 }
