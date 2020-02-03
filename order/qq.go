d8f099077355830e20d6f4a14cbc1a5f286fed57
stormlin <lin-jinting@outlook.com>
2020-02-03 17:27:52 +0800

    add new transmission order for sample "qq"

--- order/qq.go
+++ order/qq.go
@@ -21,23 +21,35 @@ var QqNetworkList = []string{
 // QqPerformanceList 是 qq 样本从 performance 中提取的传输顺序
 var QqPerformanceList = []string{
 	"index.html",
-	"qbs.js",
 	"qq_3422090a.css",
-	"jquery-1.11.1.min.js",
 	"txplayer.js",
+	"jquery-1.11.1.min.js",
 	"5d09e4c5.js",
-	"stats.js",
 	"ping.js",
 	"qq_3422090a.js",
+	"qbs.js",
 	"crystal-min.js",
 	"stgw_precon.html",
-	"ver.20170622.js",
 	"qbscomm.20150907.js",
+	"ver.20170622.js",
 	"gdtlib.20181219.js",
+	"stats.js",
 }
 
 // QqNetworkPlusList 是 qq 样本合并了 network 和 performance 两种顺序之后的结果
 var QqNetworkPlusList = []string{
 	"index.html",
-	
+	"qbs.js",
+	"qq_3422090a.css",
+	"txplayer.js",
+	"jquery-1.11.1.min.js",
+	"5d09e4c5.js",
+	"ping.js",
+	"qq_3422090a.js",
+	"crystal-min.js",
+	"stats.js",
+	"stgw_precon.html",
+	"qbscomm.20150907.js",
+	"ver.20170622.js",
+	"gdtlib.20181219.js",
 }
